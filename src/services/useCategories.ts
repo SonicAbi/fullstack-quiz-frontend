7f2cf1b6c0543e6c15b13df7d4a64f533ef380cf
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-04-07 15:06:19 +0200

    feat: refactor useCategories to handle API response errors

--- src/services/useCategories.ts
+++ src/services/useCategories.ts
@@ -1,12 +1,17 @@
-import { Category } from "@/types";
+import { ApiResponse, Category } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 export const useCategories = () =>
-    useQuery<Category[]>({
-      queryKey: ["categories"],
-      queryFn: async (): Promise<Category[]> => {
-        const response = await fetch("http://localhost:3000/categories");
-        const data  = await response.json();
-        return data
-      },
-    });
+  useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+
+const getCategories = async () => {
+  const response = await fetch("http://localhost:3000/categories");
+  const json: ApiResponse<Category[]> = await response.json();
+  if ("error" in json) {
+    throw new Error(json.error);
+  }
+  return json.data;
+};
