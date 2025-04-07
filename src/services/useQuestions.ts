156545819b2f60452cc52458c8bfb81e5197dbf6
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-04-07 15:06:31 +0200

    feat: enhance useQuestions to handle API response errors and refactor fetching logic

--- src/services/useQuestions.ts
+++ src/services/useQuestions.ts
@@ -1,14 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import { Question } from "../types";
+import { ApiResponse, Question } from "../types";
 
-export const useQuestions = (category: string) =>
+
+export const useQuestions = (category: string | undefined) =>
   useQuery<Question[]>({
     queryKey: ["questions", category],
-    queryFn: async (): Promise<Question[]> => {
-      const response = await fetch(
-        `http://localhost:3000/questions?category=${category}`,
-      );
-      const data = await response.json();
-      return data;
-    },
+    queryFn: () => getQuestions(category),
   });
+
+const getQuestions = async (category: string | undefined) => {
+  const response = await fetch(
+    `http://localhost:3000/questions?category=${category}`,
+  );
+  const json: ApiResponse<Question[]> = await response.json();
+  if ("error" in json) {
+    throw new Error(json.error);
+  }
+  return json.data;
+};
