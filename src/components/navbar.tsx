52686b7e41e05f7dd89495bd0f2131537cf5f4ce
Mehmet Erdem Koc <mehmet.erdem.koc@volkswagen.de>
2025-04-07 15:06:55 +0200

    feat: update Navbar component to make the title a clickable link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
     <nav className="bg-gradient-to-l from-myLightTurquoise to-violet-500 p-4 shadow-lg">
       <div className="container mx-auto flex items-center justify-between">
         <h1 className="text-2xl font-bold text-myLightGray underline">
-          Learning website only for G2 & G3✌🏼
+          <Link to={"/"}> Learning website only for G2 & G3✌🏼</Link>
         </h1>
         <div className="flex space-x-4">
           <Link to='/' className="px-4 py-2 rounded-md font-medium">
